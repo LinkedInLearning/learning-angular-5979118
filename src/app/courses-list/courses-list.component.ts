609fcf57ffaa1d6925f684d22a99297117f1b003
maaikevanputten <maaikevanputten@maaike.nl>
2025-01-29 10:02:36 +0100

    wishlish feature

--- src/app/courses-list/courses-list.component.ts
+++ src/app/courses-list/courses-list.component.ts
@@ -9,6 +9,7 @@ import { CourseCardComponent } from '../course-card/course-card.component';
 })
 export class CoursesListComponent implements OnInit {
   title: string = "Available courses";
+  wishlist: any[] = [];
   courses = [
     { id: 1, title: 'Intro to Angular', description: 'Learn the basics of Angular', price: 49, date: '2025-03-01', soldOut: false, img: 'angular-logo.png', onSale: false },
     { id: 2, title: 'Advanced Angular', description: 'Deep dive into Angular internals', price: 99, date: '2025-04-10', soldOut: true, img: 'angular-logo.png', onSale: true },
@@ -24,5 +25,8 @@ export class CoursesListComponent implements OnInit {
     // Potentially do more here in the future
   }
 
- 
+  onWishlistAdded(course: any) {
+    console.log('Wishlist event triggered for:', course.title);
+    this.wishlist.push(course);
+  }
 }
