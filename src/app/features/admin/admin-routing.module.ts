f9041bf6caeefe3603a076e50fd48261c0bc7cd7
maaikevanputten <maaikevanputten@maaike.nl>
2025-02-16 12:58:21 +0100

    dashboard

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddNewCourseComponent } from './add-new-course/add-new-course.component';
 import { AppComponent } from '../../app.component';
 import { StudentListComponent } from './student-list/student-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent,
+    component: DashboardComponent,
     children: [
       { path: 'new-course', component: AddNewCourseComponent},
       { path: 'students', component: StudentListComponent},
+      { path: '', redirectTo: 'students', pathMatch: 'full'}
     ]
   }
 ];
